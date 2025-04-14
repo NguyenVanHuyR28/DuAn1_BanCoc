56defab0a20d3cee07feff9b2d65afb9fdfc9cd5
Huy Nguyen <vanhuynguyen2802@gmail.com>
2025-04-14 22:06:14 +0700

    binh luan, quan ly binh luan

---
+++ admin/controllers/AdminBinhLuanController.php
@@ -0,0 +1,42 @@
+<?php
+class AdminBinhLuanController
+{
+    public $modelBinhLuan;
+    public function __construct()
+    {
+        $this->modelBinhLuan = new AdminBinhLuan();
+    }
+
+    public function hideBinhLuan() 
+    {
+        $bl_id = $_GET['id'];
+        if($bl_id !== null) {
+            $this->modelBinhLuan->hideBinhLuan($bl_id);
+            header('Location: ' . BASE_URL_ADMIN . '?act=listBinhLuan');
+            exit();
+        }else {
+            die("ID bình luận không hợp lệ.");
+        }
+    }
+
+    public function showBinhLuan()
+    {
+        $bl_id = $_GET['id'];
+        if($bl_id !== null) {
+            $this->modelBinhLuan->showBinhLuan($bl_id);
+            header('Location: ' . BASE_URL_ADMIN . '?act=listBinhLuan');
+            exit();
+    }else{
+            die("ID bình luận không hợp lệ.");
+        }
+    }
+    public function listBinhLuan()
+    {
+        // Lấy tất cả bình luận
+        $sp_id = $_GET['sp_id'] ?? null; // Lấy ID sản phẩm từ URL (nếu có)
+        $listBinhLuan = $this->modelBinhLuan->getAllComment($sp_id); // Truyền ID sản phẩm để lọc
+    
+        // Gọi view hiển thị danh sách bình luận
+        require_once "./views/binhluan/listBinhLuan.php";
+    }
+}
